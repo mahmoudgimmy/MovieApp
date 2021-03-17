4fc61bdb0d13c4cec49cfb10376b13918d398717
Mahmoud Gamal <mahmoud.gamal2791996@gmail.com>
2021-03-17 12:49:51 +0200

    - initial commits

---
+++ app/src/test/java/com/example/movieapp/MovieDetailsViewModelTest.kt
@@ -0,0 +1,28 @@
+package com.example.movieapp
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.example.movieapp.ui.moviedetails.repos.MovieDetailsRepository
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.junit.Before
+import org.junit.Rule
+import org.junit.rules.TestRule
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.junit.MockitoJUnitRunner
+
+@ExperimentalCoroutinesApi
+@RunWith(MockitoJUnitRunner::class)
+class MovieDetailsViewModelTest {
+    @get:Rule
+    val testInstantTaskExecutorRule: TestRule = InstantTaskExecutorRule()
+
+    @get:Rule
+    val testCoroutineRule = TestCoroutineRule()
+    @Mock
+    private lateinit var repository: MovieDetailsRepository
+
+    @Before
+    fun setUp() {
+        // do something if required
+    }
+}
